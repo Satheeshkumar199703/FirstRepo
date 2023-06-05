9343bd4e465a24e9118c1eb291701d0604f83b58
Satheeshkumar199703 <satheeshkaifen@gmail.com>
2023-06-05 21:57:16 +0530

    First Commit

---
+++ src/javaPackage/JavaTraining.java
@@ -0,0 +1,59 @@
+package javaPackage;
+
+import java.util.*;
+
+public class JavaTraining {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		char[] s1= s.toCharArray();
+		int count=0;
+		Map<Character,Integer> map= new HashMap<>();
+		
+		for(int i=0;i<s1.length;i++) {
+			count=0;
+			for(int j=0;j<s1.length;j++)
+			{
+				if(s1[i]==s1[j])
+				{
+					count++;
+				}
+			}
+			map.put(s1[i], count);
+		}
+		System.out.println(map);
+		
+		
+		
+	}
+
+}
+//	public static void main(String[] args) {
+//		
+//	
+//	
+//	Scanner sc = new Scanner(System.in);
+//	String s=sc.nextLine();
+//	char[] s1=s.toCharArray();
+//	int count=0;
+//	
+//	Map <Character,Integer>map = new TreeMap<>() ;
+//	
+//	for(int i=0;i<s1.length;i++)
+//	{	count=0;	
+//		for(int j=0;j<s1.length;j++)
+//		{
+//			
+//			if(s1[i]==s1[j])
+//			{
+//				count++;
+//			}
+//		}
+//		map.put(s1[i], count);
+//	}
+//	System.out.println(map);
+//	
+//
+//}
+
